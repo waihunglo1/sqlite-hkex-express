5bdb383d2064ca83e94c04b5bd86774e781620e8
stayinvest-for-dummy <waihunglo@gmail.com>
2026-02-19 12:08:44 +0800

    fix(utility): Remove unused BeautifulSoup import from utility.py

--- scripts/utility.py
+++ scripts/utility.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
 from datetime import date, timedelta
 import os
 import datetime
